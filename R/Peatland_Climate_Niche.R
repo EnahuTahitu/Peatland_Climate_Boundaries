f881947b06dbb482d922a25fd94ac277ddea6076
EnahuTahitu <52380714+EnahuTahitu@users.noreply.github.com>
2025-04-09 10:28:38 +0200

    Add Peatland_Climate_Niche.R script to R directory

---
+++ R/Peatland_Climate_Niche.R
@@ -0,0 +1,8 @@
+# Final model creation
+# Date: 25/09/2024
+# Author: Enahu Tahitu
+
+# Importing and cleaning data
+# ...existing code from Peatland_Climate_Niche.R...
+
+# Add your R script content here.
